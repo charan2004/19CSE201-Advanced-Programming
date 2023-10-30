08f3c048c5993f9247ef73b6e61794a155916b10
Charan Kanagarajan <52920998+charan2004@users.noreply.github.com>
2023-10-30 16:16:06 +0530

    30-10-2023 Python AP

---
+++ Python_AP/functions22018.py
@@ -0,0 +1,13 @@
+cys=['vyshali','charan','hansica']
+cys.insert(0,'amritha')
+print(cys)
+cys.append('deepak')
+print(cys)
+cys.pop(0)
+print(cys)
+cys.remove('deepak')
+print(cys)
+cys.clear()
+print(cys)
+del cys
+print(cys)
